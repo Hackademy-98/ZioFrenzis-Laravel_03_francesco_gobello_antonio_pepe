1e09f33ceaf4de3ec62c90810647bf8f5739762a
Andrea Cibin <andreacibin@MBP-di-Andrea.Home>
2023-10-31 16:58:14 +0100

    prima Route finita

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/', function () {
     
     return view('index');
 });
+
+Route::get('/Games', function(){
+    $videogames = [
+        ["name" => "Final Fantasy VII Rebirth", "prezzo" => "80,98€", "Genere" => "Action/Adventure"],
+        ["name" => "Fifa 24", "prezzo" => "69,98€", "Genere" => "Sport"],
+        ["name" => "Call of Duty", "prezzo" => "100€", "Genere" => "Spara Tutto"],
+        ["name" => "Rachet & Clank", "prezzo" => "50,45€", "Genere" => "Fantasy/Adventure"],
+        ["name" => "Spiderman 2", "prezzo" => "79,99€", "Genere" => "Supereroi"]
+    ];
+    return view('Games', ['videogames'=>$videogames]);
+})->name('Videogames');
+
